96874f5ab5a4e270975184b35b1f9f33ba565315
DmitriySi <demonishishe@gmail.com>
2022-05-13 21:15:10 +0300

    add

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -12,16 +12,17 @@ public class Main {
     public static void main(String[] args) throws SQLException {
         // реализуйте алгоритм здесь
 
-        UserDao userDao = new UserDaoJDBCImpl();
+       // UserDao userDao = new UserDaoJDBCImpl();
+        UserDao hibernate = new UserDaoHibernateImpl();
 
-        userDao.dropUsersTable();
-        userDao.createUsersTable();
-        userDao.saveUser("Ivan", "Petrov", (byte) 34);
-        userDao.saveUser("Oleg", "Ivanov", (byte) 45);
-        userDao.saveUser("Ben", "Ivanov", (byte) 23);
-        userDao.saveUser("Boris", "Ivanov", (byte) 64);
-        userDao.getAllUsers().forEach(System.out::println);
-        userDao.cleanUsersTable();
+        hibernate.dropUsersTable();
+        hibernate.createUsersTable();
+        hibernate.saveUser("Ivan", "Petrov", (byte) 34);
+        hibernate.saveUser("Oleg", "Ivanov", (byte) 45);
+        hibernate.saveUser("Ben", "Ivanov", (byte) 23);
+        hibernate.saveUser("Boris", "Ivanov", (byte) 64);
+        hibernate.getAllUsers().forEach(System.out::println);
+        hibernate.cleanUsersTable();
         try {
             Util.getConnection().commit();//про коммиты не совсем понял я
             Util.getConnection().close();
--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -12,31 +12,31 @@ public class UserServiceImpl implements UserService {
     private final UserDao userDaoHibernate = new UserDaoHibernateImpl();
     public void createUsersTable() {
         userDaoJDBC.createUsersTable();
-        //userDaoHibernate.createUsersTable();
+        userDaoHibernate.createUsersTable();
     }
 
     public void dropUsersTable() {
         userDaoJDBC.dropUsersTable();
-        //userDaoHibernate.dropUsersTable();
+        userDaoHibernate.dropUsersTable();
     }
 
     public void saveUser(String name, String lastName, byte age) {
         userDaoJDBC.saveUser(name, lastName, age);
-        //userDaoHibernate.saveUser(name, lastName, age);
+        userDaoHibernate.saveUser(name, lastName, age);
     }
 
     public void removeUserById(long id) {
         userDaoJDBC.removeUserById(id);
-        //userDaoHibernate.removeUserById(id);
+        userDaoHibernate.removeUserById(id);
     }
 
     public List<User> getAllUsers() {
-        return userDaoJDBC.getAllUsers();
-       // return userDaoHibernate.getAllUsers();
+        //return userDaoJDBC.getAllUsers();
+        return userDaoHibernate.getAllUsers();
     }
 
     public void cleanUsersTable() {
         userDaoJDBC.cleanUsersTable();
-        //userDaoHibernate.cleanUsersTable();
+        userDaoHibernate.cleanUsersTable();
     }
 }
